167f168eb46a96a33a9971adee0edfd38a0664f4
Douglas Maxwell <dsm1015@sru.edu>
2023-02-09 14:30:45 -0500

    Spring Boot/Maven environment

---
+++ src/main/java/edu/sru/dsm1015/TrafficSim/TrafficSimApplication.java
@@ -0,0 +1,13 @@
+package edu.sru.dsm1015.TrafficSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficSimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sru/dsm1015/TrafficSim/TrafficSimApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sru.dsm1015.TrafficSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
